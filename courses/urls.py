53251c688f2d19cf0c78adc7f42dd3f0ceac70fc
Emmet1337 <sardazadr@gmail.com>
2019-11-16 23:02:38 +0600

    ц

--- courses/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from courses import views
-
-urlpatterns = [
-    path('courses/', views.CourseListView.as_view()),
-]
--- courses/views.py
+++
@@ -1,8 +0,0 @@
-from courses.models import Course
-from courses.serializers import CourseSerializer
-from rest_framework import generics
-
-
-class CourseListView(generics.ListAPIView):
-    serializer_class = CourseSerializer
-    queryset = Course.objects.all()
